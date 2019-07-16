b37c0c50003b5a27ded373ba3bed48e907b4acbf
Wallace Carr <wallacecarr4@gmail.com>
2019-07-16 16:59:57 -0600

    init commit

---
+++ tutorial.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+func main() {
+	fmt.Println("hello world")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println(true && false)
+	var a = "initial"
+	f:= "apple"
+	fmt.Println(a, f)
+}
